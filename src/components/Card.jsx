5738a55ec08eefee95b5657e486450e44a3665a8
abduvosidvaliyev <valiyevabduvosid6761@gmail.com>
2024-12-06 15:10:36 +0500

    thj

---
+++ src/components/Card.jsx
@@ -0,0 +1,34 @@
+const Card = () => {
+    return (
+        <>
+            <div className="galleryn">
+                <div>
+                    <img src={image1} alt="" />
+                </div>
+                <div>
+                    <img src={image2} alt="" />
+                </div>
+                <div>
+                    <img src={image3} alt="" />
+                </div>
+                <div>
+                    <img src={image4} alt="" />
+                </div>
+                <div>
+                    <img src={image5} alt="" />
+                </div>
+                <div>
+                    <img src={image6} alt="" />
+                </div>
+                <div>
+                    <img src={image7} alt="" />
+                </div>
+                <div>
+                    <img src={image8} alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card
